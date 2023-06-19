f8daf5b1e5d5e9c6e40d8849e2e5b8dca1b189bb
kamarini <kama0549@stud.kea.dk>
2023-06-19 20:09:05 +0200

    npm install

--- src/pages/tickets/index.jsx
+++ src/pages/tickets/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import  { useContext } from "react";
 import FlowLayout from "@/components/FlowLayout";
 import TicketCard from "@/components/TicketCard";
 import { StoreContext } from "@/context/storeContext";
