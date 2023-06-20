bc607b0d2416397a4fb8d425f2b28dc5b032a095
kamarini <kama0549@stud.kea.dk>
2023-06-20 14:58:53 +0200

    added comments

--- src/context/TimerContext.jsx
+++ src/context/TimerContext.jsx
@@ -4,21 +4,23 @@ import { StoreContext } from "@/context/storeContext";
 const TimerContext = createContext();
 
 function TimerProvider({ children }) {
+  //im getting the array reserveSpot from the StoreContext
   const { reserveSpot } = useContext(StoreContext);
+  
+  //getting the timeout=300000 from the database
   const timeout = reserveSpot[0]?.timeout || 0;
+
   const [timeRemaining, setTimeRemaining] = useState(timeout);
 
 
   //If the timeout value in your StoreContext changes dynamically and you want to update the 
   //timeRemaining value in the TimerProvider accordingly, you can modify the TimerProvider 
   //component to include a useEffect hook that listens for changes in the timeout value.
-  // useEffect(() => {
-  //   setTimeRemaining(timeout); // Update timeRemaining when timeout changes
-  // }, [timeout]);
- console.log("RESERVESPOT", timeout);
+  
+//  console.log("RESERVESPOT", timeout);
   useEffect(() => {
     if (reserveSpot.length > 0) {
-      setTimeRemaining(timeout);
+      setTimeRemaining(timeout);// Update timeRemaining when timeout changes
     }
   }, [reserveSpot, timeout]);
 
