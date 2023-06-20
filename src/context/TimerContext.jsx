3b14247230be3719f86d8843a3960f016fd28c13
kamarini <kama0549@stud.kea.dk>
2023-06-20 14:46:06 +0200

    added condition for timeout

--- src/context/TimerContext.jsx
+++ src/context/TimerContext.jsx
@@ -12,9 +12,15 @@ function TimerProvider({ children }) {
   //If the timeout value in your StoreContext changes dynamically and you want to update the 
   //timeRemaining value in the TimerProvider accordingly, you can modify the TimerProvider 
   //component to include a useEffect hook that listens for changes in the timeout value.
+  // useEffect(() => {
+  //   setTimeRemaining(timeout); // Update timeRemaining when timeout changes
+  // }, [timeout]);
+ console.log("RESERVESPOT", timeout);
   useEffect(() => {
-    setTimeRemaining(timeout); // Update timeRemaining when timeout changes
-  }, [timeout]);
+    if (reserveSpot.length > 0) {
+      setTimeRemaining(timeout);
+    }
+  }, [reserveSpot, timeout]);
 
   return (
     <TimerContext.Provider value={{ timeRemaining, setTimeRemaining }}>
