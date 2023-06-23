44046c96454cc1fc0969b5f41eb8892cb9a42d2b
kamarini <kama0549@stud.kea.dk>
2023-06-23 02:48:51 +0200

    its working!

--- src/pages/all-bands/index.jsx
+++ src/pages/all-bands/index.jsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 import Anchor from "@/components/Anchor";
 
-export default function MondayBands() {
+export default function MyPage() {
   const [schedule, setSchedule] = useState(null);
   const [bands, setBands] = useState(null);
   const router = useRouter();
@@ -46,63 +46,75 @@ export default function MondayBands() {
     );
   }
 
-  const midgardActs = schedule.Midgard.mon;
-  const vanaheimActs = schedule.Vanaheim.mon;
-  const jotunheimActs = schedule.Jotunheim.mon;
-
-  const filteredActs = [
-    ...midgardActs,
-    ...vanaheimActs,
-    ...jotunheimActs
-  ].filter((act) => act.act.toLowerCase() !== "break");
-
-  const bandNames = bands
-    .filter((band) =>
-      filteredActs.some((act) => act.act.toLowerCase() === band.name.toLowerCase())
-    )
-    .map((band) => band.name);
+  const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+  const fullDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
   return (
     <Layout>
-      <section className="flex flex-col justify-between px-10 h-full">
+     <section className="flex flex-col justify-between px-10 h-full">
         <div>
           <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-white py-4 pt-10 mt-[5rem]">
-            Bands Playing on Monday
+            Line-up
           </h1>
           <h2 className="text-2xl md:text-2xl text-white font-light pb-10 w-full lg:w-1/2">
-            Here are the bands that are playing on Monday in Midgard, Vanaheim, and Jotunheim.
+            Here is the schedule of bands for each day in Midgard, Vanaheim, and Jotunheim.
           </h2>
         </div>
       </section>
+
+
       <section className="container mx-auto p-4 text-center">
-        <section className="container mx-auto p-4 text-center">
-          <div className="text-center">
-            {bandNames.map((name, index) => {
-              const band = bands.find((band) => band.name.toLowerCase() === name.toLowerCase());
+        {days.map((day, index) => {
+          const midgardActs = schedule.Midgard[day];
+          const vanaheimActs = schedule.Vanaheim[day];
+          const jotunheimActs = schedule.Jotunheim[day];
 
-              if (!band) {
-                return null;
-              }
+          const filteredActs = [
+            ...midgardActs,
+            ...vanaheimActs,
+            ...jotunheimActs
+          ].filter((act) => act.act.toLowerCase() !== "break");
 
-              let textStyle;
-              if (index < 4) {
-                textStyle = "text-5xl font-bold";
-              } else if (index < 12) {
-                textStyle = "text-2xl font-bold";
-              } else {
-                textStyle = "text-lg";
-              }
+          const bandNames = bands
+            .filter((band) =>
+              filteredActs.some((act) => act.act.toLowerCase() === band.name.toLowerCase())
+            )
+            .map((band) => band.name);
 
-              return (
-                <div key={index} className={`cursor-pointer ${textStyle} my-2`}>
-                  <a href={`/bands/${band.slug}`}>
-                    {name}
-                  </a>
-                </div>
-              );
-            })}
-          </div>
-        </section>
+          return (
+            <section key={index} className="container mx-auto p-4 text-center">
+              <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-white py-4">
+                {fullDays[index]}
+              </h1>
+              <div className="text-center">
+                {bandNames.map((name, index) => {
+                  const band = bands.find((band) => band.name.toLowerCase() === name.toLowerCase());
+
+                  if (!band) {
+                    return null;
+                  }
+
+                  let textStyle;
+                  if (index < 4) {
+                    textStyle = "text-5xl font-bold";
+                  } else if (index < 12) {
+                    textStyle = "text-2xl font-bold";
+                  } else {
+                    textStyle = "text-lg";
+                  }
+
+                  return (
+                    <div key={index} className={`cursor-pointer ${textStyle} my-2`}>
+                      <a href={`/bands/${band.slug}`}>
+                        {name}
+                      </a>
+                    </div>
+                  );
+                })}
+              </div>
+            </section>
+          );
+        })}
       </section>
     </Layout>
   );
